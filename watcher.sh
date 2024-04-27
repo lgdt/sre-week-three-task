a2bd44a118c13ed75808f640485a06a8f4b1410d
Marat Karakhanov <marat.karakhanov@gmail.com>
2024-04-27 02:17:41 +0000

    [Week 3] - Watcher script that scales down the app to 0 if it keeps crashing

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,13 @@
-#TODO
+NS=sre
+DEPLOY=swype-app
+MAX_RESTARTS=3
+
+while true
+do
+    num_restarts=$(kubectl get pods -n $NS | grep $DEPLOY | awk '{print $4}')
+    echo "Number of restarts of $DEPLOY app: $num_restarts"
+    if [ "$num_restarts" -gt "$MAX_RESTARTS" ]; then
+        kubectl scale deployment $DEPLOY -n $NS --replicas=0
+    fi
+    sleep 60
+done
